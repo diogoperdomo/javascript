4faf97f729b5fb622b2e67a735c4de957e02ca1e
Diogo de Castro Perdomo <diogo.perdomo@gmail.com>
2018-10-29 12:54:20 -0300

    Update arrayDiff.js

--- arrayDiff.js
+++ arrayDiff.js
@@ -4,7 +4,9 @@ Array.prototype.diff = function(a){
             return A.indexOf(JSON.stringify(i))<0})
 }
 
-var array_1 = [{a:1,b:2},{a:2,b:3}];
-var array_2 = [{a:2,b:3},{a:3,b:4},{a:4,b:5}];
-var newarray = array_2.diff(array_1);
-console.log(newarray);//--> [ { a: 3, b: 4 }, { a: 4, b: 5 } ]
+//EXAMPLE:
+//var array_1 = [{a:1,b:2},{a:2,b:3}];
+//var array_2 = [{a:2,b:3},{a:3,b:4},{a:4,b:5}];
+//var newarray = array_2.diff(array_1);
+//console.log(newarray);
+//--> [ { a: 3, b: 4 }, { a: 4, b: 5 } ]
